08fdb4ea1742cc0e8c0c5fc6bcbc79e2b10be1a6
Emmanuel Carreira <emmanuel_carreira@hotmail.com>
2020-12-15 23:22:34 -0300

    refactor: modify bot.py to use bot class already implemented in discord.py

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,29 @@
 import os
 
-from custom_client import CustomClient
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = CustomClient()
-client.run(TOKEN)
+bot = commands.Bot(command_prefix='#')
+
+
+@bot.command(name='hello', help='Displays a friendly message')
+async def hello(context):
+    await context.send('Hello to you!')
+
+
+@bot.command(name='echo', help='Echoes the message back')
+async def echo(context, message : str):
+    await context.send(message)
+
+
+@commands.has_role('camarada')
+@bot.command(name='comrade', help='Comrades unite!')
+async def comrade(context):
+    await context.send('Hello comrade!')
+
+
+
+bot.run(TOKEN)
